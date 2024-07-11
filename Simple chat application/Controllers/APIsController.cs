9ff018eb5fa833c74c64e4bdaac8d55e8200e2e1
Yuriy Kryvushin <krivyshinnoq@gmail.com>
2024-07-12 00:21:19 +0300

    Add project files.

---
+++ Simple chat application/Controllers/APIsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_chat_application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIsController : ControllerBase
+    {
+    }
+}
