a9a5e195af4513f0ea65d77929a27e75ef0bd16a
Yuriy Kryvushin <krivyshinnoq@gmail.com>
2024-07-12 19:57:04 +0300

    fixed mandatory user parameters

--- Simple chat application/Model/User.cs
+++ Simple chat application/Model/User.cs
@@ -3,7 +3,8 @@
     public class User
     {
         public Guid UserId { get; set; }
-        public ICollection<UserChat> UserChats { get; set; }
-        public ICollection<Message> Messages { get; set; }
+        public string UserName { get; set; }
+        public ICollection<UserChat> UserChats { get; set; } = new List<UserChat>();
+        public ICollection<Message> Messages { get; set; } = new List<Message>();
     }
 }
